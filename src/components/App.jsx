5c0cb72797120fd82319384b27993a8f65c0a0b7
mcbouslog <mcbouslog@gmail.com>
2018-10-23 18:07:23 -0500

    Init add recents pagination

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,15 @@ import * as GrommetApp from 'grommet/components/App';
 import Box from 'grommet/components/Box';
 import { ZooFooter, ZooHeader } from 'zooniverse-react-components';
 
-import { withUser } from '../context/UserContext';
+import { UserProvider, withUser } from '../context/UserContext';
 import AuthContainer from '../containers/AuthContainer';
-import FavoritesContainer from '../containers/FavoritesContainer';
-import HistogramContainer from '../containers/HistogramContainer';
-import RecentsContainer from '../containers/RecentsContainer';
 import UserHeading from './UserHeading';
+import RecentsContainer from '../containers/RecentsContainer';
+import FavoritesContainer from '../containers/FavoritesContainer';
 import UserStatsContainer from '../containers/UserStatsContainer';
+import HistogramContainer from '../containers/HistogramContainer';
 
-import Title from './Title'; // remove with Badge section
+import Title from './Title'; // remove with completion of Badge section
 
 const UserHeadingWithUser = withUser(UserHeading);
 const RecentsWithUser = withUser(RecentsContainer);
@@ -20,37 +20,39 @@ const UserStatsWithUser = withUser(UserStatsContainer);
 const HistogramWithUser = withUser(HistogramContainer);
 
 const App = () => (
-  <GrommetApp centered={false}>
-    <ZooHeader authContainer={<AuthContainer />} />
-    <Box className="main" pad="large" tag="main">
-      <UserHeadingWithUser />
-      <Box direction="row" full="horizontal" margin={{ bottom: 'medium' }}>
+  <UserProvider>
+    <GrommetApp centered={false}>
+      <ZooHeader authContainer={<AuthContainer />} />
+      <Box className="main" pad="large" tag="main">
+        <UserHeadingWithUser />
+        <Box direction="row" full="horizontal" margin={{ bottom: 'medium' }}>
+          <Box
+            basis="2/3"
+            colorIndex="light-1"
+            margin={{ right: 'medium' }}
+            pad="medium"
+          >
+            <RecentsWithUser />
+            <hr className="main__hr" />
+            <FavoritesWithUser />
+          </Box>
+          <Box basis="1/3" justify="between">
+            <UserStatsWithUser />
+            <HistogramWithUser />
+          </Box>
+        </Box>
         <Box
-          basis="2/3"
           colorIndex="light-1"
-          margin={{ right: 'medium' }}
+          full="horizontal"
           pad="medium"
+          style={{ height: '250px' }}
         >
-          <RecentsWithUser />
-          <hr className="main__hr" />
-          <FavoritesWithUser />
-        </Box>
-        <Box basis="1/3" justify="between">
-          <UserStatsWithUser />
-          <HistogramWithUser />
+          <Title>Your Badges</Title>
         </Box>
       </Box>
-      <Box
-        colorIndex="light-1"
-        full="horizontal"
-        pad="medium"
-        style={{ height: '250px' }}
-      >
-        <Title>Your Badges</Title>
-      </Box>
-    </Box>
-    <ZooFooter />
-  </GrommetApp>
+      <ZooFooter />
+    </GrommetApp>
+  </UserProvider>
 );
 
 export default App;
--- src/containers/RecentsContainer.jsx
+++ src/containers/RecentsContainer.jsx
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import Box from 'grommet/components/Box';
+import { Paginator } from 'zooniverse-react-components';
+
 import { config } from '../config';
 import Title from '../components/Title';
 import SubjectCard from '../components/SubjectCard';
@@ -9,8 +11,11 @@ class RecentsContainer extends React.Component {
   constructor() {
     super();
     this.state = {
+      meta: {},
       recents: []
     };
+
+    this.onPageChange = this.onPageChange.bind(this);
   }
 
   componentDidMount() {
@@ -23,21 +28,23 @@ class RecentsContainer extends React.Component {
     }
   }
 
-  fetchRecents() {
+  onPageChange(page) {
+    this.fetchRecents(page);
+  }
+
+  fetchRecents(page = 1) {
     const { user } = this.props;
+    const query = {
+      project_id: config.projectId,
+      sort: '-created_at',
+      page,
+      page_size: 3
+    };
 
     if (user && user.get) {
-      user
-        .get('recents', {
-          project_id: config.projectId,
-          sort: '-created_at',
-          page: this.state.page,
-          page_size: 3
-        })
-        .then(recents => {
-          this.setState({ recents });
-          console.log('meta = ', recents[0].getMeta());
-        });
+      user.get('recents', query).then(recents => {
+        this.setState({ meta: recents[0].getMeta(), recents });
+      });
     }
   }
 
@@ -51,6 +58,14 @@ class RecentsContainer extends React.Component {
               <SubjectCard key={recent.id} subject={recent} />
             ))}
         </Box>
+        {this.state.meta &&
+          this.state.meta.page_count > 1 && (
+            <Paginator
+              page={this.state.meta.page}
+              pageCount={this.state.meta.page_count}
+              onPageChange={this.onPageChange}
+            />
+          )}
       </Box>
     );
   }
