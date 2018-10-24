20a161e721f333e1dddb206c3a62ff25d3810667
mcbouslog <mcbouslog@gmail.com>
2018-10-24 01:51:31 -0500

    link to talk subject

--- src/components/SubjectCard.jsx
+++ src/components/SubjectCard.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import Box from 'grommet/components/Box';
 import Button from 'grommet/components/Button';
 import { Thumbnail } from 'zooniverse-react-components';
+
+import { config } from '../config';
 import getSubjectLocations from '../lib/get-subject-locations';
 
 export default function SubjectCard({ subject }) {
@@ -17,16 +19,28 @@ export default function SubjectCard({ subject }) {
     type = 'video';
     [format, src] = locations.video;
   }
+
+  const subjectId = subject.links.subject
+    ? `/subjects/${subject.links.subject}`
+    : subject.href;
   return (
-    <Box alignSelf="end" className="subject-card" margin="small">
-      <Thumbnail
-        alt={`Subject ${subject.links.subject}`}
-        src={src}
-        type={type}
-        format={format}
-        height={250}
-        width={200}
-      />
+    <Box alignSelf="center" className="subject-card" margin="small">
+      <a
+        href={`${config.zooniverse}/projects/${
+          config.projectSlug
+        }/talk${subjectId}`}
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <Thumbnail
+          alt={`Subject ${subject.links.subject}`}
+          src={src}
+          type={type}
+          format={format}
+          height={250}
+          width={200}
+        />
+      </a>
       <Box direction="row" justify="center">
         <Button type="button">
           <i className="fa fa-heart fa-fw" />
