63badffeba57c0e4d678d82f879c2d3e4624691d
mcbouslog <mcbouslog@gmail.com>
2018-10-31 14:52:29 -0500

    Add projectId param

--- src/config.js
+++ src/config.js
@@ -30,11 +30,13 @@ if (!env.match(/^(production|staging|development)$/)) {
   throw new Error(`Error: Invalid Environment - ${env}`);
 }
 
+const projectIdFromBrowser = locationMatch(/\W?project=(\w+)/);
+
 const baseConfig = {
   development: {
     caesar: 'https://caesar-staging.zooniverse.org',
     origin: window.location.origin,
-    projectId: '1613',
+    projectId: projectIdFromBrowser || '1613',
     projectSlug: 'rafe-dot-lafrance/notes-from-nature-on-staging',
     panoptesAppId:
       '16ac801e4ad438d929d30668206df31294e7a7222ce3f449a1c4b45cd80d44cc',
@@ -46,7 +48,7 @@ const baseConfig = {
     origin: window.location.origin,
     panoptesAppId:
       '940c60fa02251f156c0d9129e3b8d885424fe729da4e4a7750525ca154bdb3d7',
-    projectId: '1558',
+    projectId: projectIdFromBrowser || '1558',
     projectSlug: 'zooniverse/notes-from-nature',
     stats: 'https://stats.zooniverse.org',
     zooniverse: 'https://www.zooniverse.org'
