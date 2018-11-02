a3a267f4d4ada9719da5559251575f4b1770e9b6
mcbouslog <mcbouslog@gmail.com>
2018-11-02 17:25:27 -0500

    Add project context, refactor user context

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,8 @@ import GrommetApp from 'grommet/components/App';
 import Box from 'grommet/components/Box';
 import { ZooFooter, ZooHeader } from 'zooniverse-react-components';
 
-import { UserProvider, withUser } from '../context/UserContext';
+import { ProjectProvider, ProjectContext } from '../context/ProjectContext';
+import { UserProvider, UserContext } from '../context/UserContext';
 import AuthContainer from '../containers/AuthContainer';
 import UserHeading from './UserHeading';
 import RecentsContainer from '../containers/RecentsContainer';
@@ -12,46 +13,54 @@ import StatsContainer from '../containers/StatsContainer';
 
 import Title from './Title'; // remove with completion of Badge section
 
-const UserHeadingWithUser = withUser(UserHeading);
-const RecentsWithUser = withUser(RecentsContainer);
-const FavoritesWithUser = withUser(FavoritesContainer);
-const StatsWithUser = withUser(StatsContainer);
-
 const App = () => (
   <UserProvider>
-    <GrommetApp centered={false}>
-      <ZooHeader authContainer={<AuthContainer />} />
-      <Box className="main" pad="large" tag="main">
-        <UserHeadingWithUser />
-        <Box
-          direction="row"
-          full="horizontal"
-          margin={{ bottom: 'medium' }}
-          responsive
-        >
-          <Box
-            basis="2/3"
-            colorIndex="light-1"
-            justify="between"
-            margin={{ right: 'medium' }}
-          >
-            <RecentsWithUser />
-            <hr className="main__hr" />
-            <FavoritesWithUser />
-          </Box>
-          <StatsWithUser />
-        </Box>
-        <Box
-          colorIndex="light-1"
-          full="horizontal"
-          pad="medium"
-          style={{ height: '250px' }}
-        >
-          <Title>Your Badges</Title>
-        </Box>
-      </Box>
-      <ZooFooter />
-    </GrommetApp>
+    <ProjectProvider>
+      <UserContext.Consumer>
+        {userContext => (
+          <GrommetApp centered={false}>
+            <ZooHeader authContainer={<AuthContainer />} />
+            <Box className="main" pad="large" tag="main">
+              <ProjectContext.Consumer>
+                {projectContext => (
+                  <UserHeading
+                    project={projectContext.project}
+                    user={userContext.user}
+                  />
+                )}
+              </ProjectContext.Consumer>
+              <Box
+                direction="row"
+                full="horizontal"
+                margin={{ bottom: 'medium' }}
+                responsive
+              >
+                <Box
+                  basis="2/3"
+                  colorIndex="light-1"
+                  justify="between"
+                  margin={{ right: 'medium' }}
+                >
+                  <RecentsContainer user={userContext.user} />
+                  <hr className="main__hr" />
+                  <FavoritesContainer user={userContext.user} />
+                </Box>
+                <StatsContainer user={userContext.user} />
+              </Box>
+              <Box
+                colorIndex="light-1"
+                full="horizontal"
+                pad="medium"
+                style={{ height: '250px' }}
+              >
+                <Title>Your Badges</Title>
+              </Box>
+            </Box>
+            <ZooFooter />
+          </GrommetApp>
+        )}
+      </UserContext.Consumer>
+    </ProjectProvider>
   </UserProvider>
 );
 
